d99eaca6fd73cc2c2a13fd62ec35258d05a286ed
Itallo Ramon <irvp@ic.ufal.br>
2024-02-18 14:08:39 -0300

    feat: create the water

--- game.ts
+++ game.ts
@@ -1,4 +1,5 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from "three";
+import { Scene, PerspectiveCamera, WebGLRenderer, MirroredRepeatWrapping, PlaneGeometry, TextureLoader, Vector3 } from "three";
+import { Water } from './objects/water'
 
 export const scene = new Scene()
 export const camera = new PerspectiveCamera(
@@ -7,6 +8,24 @@ export const camera = new PerspectiveCamera(
     0.1,
     2000
 )
+
+// Configure water settings
+const waterGeometry = new PlaneGeometry(10000, 10000);
+const water = new Water(
+    waterGeometry,
+    {
+        textureWidth: 512,
+        textureHeight: 512,
+        waterNormals: new TextureLoader().load('static/normals/waternormals.jpeg', function (texture) {
+            texture.wrapS = texture.wrapT = MirroredRepeatWrapping;
+        }),
+        sunDirection: new Vector3(),
+        sunColor: 0xffffff,
+        waterColor: 0x001e0f,
+        distortionScale: 3.7,
+        fog: scene.fog !== undefined
+    }
+);
 
 // If the user changes tabs, we don't want to waste resources rendering
 const animate = () => {
@@ -23,4 +42,9 @@ async function init() {
     renderer = new WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    // Configure the rotation and position of our water plane
+    water.rotation.x = -Math.PI / 2;
+    water.rotation.z = 0;
+    scene.add(water);
 }
