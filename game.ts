c5a0e02567e31b00f2c16595cf493d26f1783c3d
Itallo Ramon <irvp@ic.ufal.br>
2024-02-18 13:58:32 -0300

    feat: create an initialization function

--- game.ts
+++ game.ts
@@ -8,5 +8,19 @@ export const camera = new PerspectiveCamera(
     2000
 )
 
+// If the user changes tabs, we don't want to waste resources rendering
+const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
 // Our three renderer
 let renderer: WebGLRenderer;
+
+// This init function will carry out the initial setup for our scene
+// and only runs once (when the game is first loaded)
+async function init() {
+    renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
