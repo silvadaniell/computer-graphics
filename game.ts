ab25f56ceca24e298b3ca63f94a9fb06c94e4487
Itallo Ramon <irvp@ic.ufal.br>
2024-02-18 14:13:59 -0300

    feat: configure the sky

--- game.ts
+++ game.ts
@@ -1,5 +1,6 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, MirroredRepeatWrapping, PlaneGeometry, TextureLoader, Vector3 } from "three";
+import { Scene, PerspectiveCamera, WebGLRenderer, MirroredRepeatWrapping, PlaneGeometry, TextureLoader, Vector3, MathUtils, PMREMGenerator, ShaderMaterial } from "three";
 import { Water } from './objects/water'
+import { Sky } from 'three/examples/jsm/objects/Sky'
 
 export const scene = new Scene()
 export const camera = new PerspectiveCamera(
@@ -8,6 +9,7 @@ export const camera = new PerspectiveCamera(
     0.1,
     2000
 )
+const sun = new Vector3();
 
 // Configure water settings
 const waterGeometry = new PlaneGeometry(10000, 10000);
@@ -47,4 +49,34 @@ async function init() {
     water.rotation.x = -Math.PI / 2;
     water.rotation.z = 0;
     scene.add(water);
+
+    // Set up the skybox
+    const sky = new Sky();
+    sky.scale.setScalar(10000); // Specify the dimensions of the skybox
+    scene.add(sky); // Add the sky to our scene
+
+    // Set up variables to control the look of the sky
+    const skyUniforms = sky.material.uniforms;
+    skyUniforms['turbidity'].value = 10;
+    skyUniforms['rayleigh'].value = 2;
+    skyUniforms['mieCoefficient'].value = 0.005;
+    skyUniforms['mieDirectionalG'].value = 0.8;
+
+    const parameters = {
+        elevation: 3,
+        azimuth: 115
+    };
+
+    const pmremGenerator = new PMREMGenerator(renderer);
+
+    const phi = MathUtils.degToRad(90 - parameters.elevation);
+    const theta = MathUtils.degToRad(parameters.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms['sunPosition'].value.copy(sun);
+    (water.material as ShaderMaterial).uniforms['sunDirection'].value.copy(sun).normalize();
+    scene.environment = pmremGenerator.fromScene(sky as any).texture;
+
+    (water.material as ShaderMaterial).uniforms['speed'].value = 0.0;
 }
